1a7e67aa2c7c72b2a0d072f394353903d2900ed8
marty-zhu <marty.zhu@gmail.com>
2022-03-27 09:12:01 -0700

    added code to play sounds on keydown

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const keys = document.querySelectorAll('div.key');
 document.addEventListener('keydown', (e) => {
     console.log(e.keyCode);
     let key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
+    let keyAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    keyAudio.play();
     key.classList.add('strike');
 });
 
