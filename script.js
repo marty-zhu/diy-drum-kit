3ec9303b752473245a1e3152c82a857ea7250fb4
marty-zhu <marty.zhu@gmail.com>
2022-03-27 08:37:03 -0700

    added event listener to highlight keys on keydown
    code listens for keydown and adds a class to the key containers with
    the corresponding keycode in the 'data-key' attr.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const keys = document.querySelectorAll('div.key');
 
-keys.forEach((key) => {
-    console.log(key.getAttribute('data-key'));
-});
+document.addEventListener('keydown', (e) => {
+    console.log(e.keyCode);
+    let key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
+    key.classList.add('strike');
+})
