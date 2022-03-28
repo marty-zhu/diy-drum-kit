9fd717e61b2ce17bc26cb3437d0939e2b10b4ecb
marty-zhu <marty.zhu@gmail.com>
2022-03-28 08:40:37 -0700

    removed conditional to check if paused on keydown
    keydown event used to first check if audio is still playing before
    resetting and play again on repeated hits. Now will just reset audio
    playhead and play.

--- script.js
+++ script.js
@@ -6,15 +6,12 @@ for (let key of keys) {
 }
 
 document.addEventListener('keydown', (e) => {
-    // console.log(e.keyCode);  // kept for easier maintenance
     if (Object.keys(keyState).includes(`${e.keyCode}`)) {
         if (!keyState[e.keyCode]) {
             let key = document.querySelector(`div.key[data-key="${e.keyCode}"]`);
             let keyAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-            if (!keyAudio.paused) {
-                keyAudio.pause();
-                keyAudio.currentTime = 0;
-            };
+
+            keyAudio.currentTime = 0;
             keyAudio.play();
             key.classList.add('strike');
             keyState[e.keyCode] = true;
